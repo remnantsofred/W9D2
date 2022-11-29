ca4b423f134ebd169bae0b1b39974f620425f09e
ByronHan333 <zhan12@dons.usfca.edu>
2022-11-29 11:43:46 -0800

    First commit

---
+++ src/moving_object.js
@@ -0,0 +1,3 @@
+//Base class for anything that moves.
+// Most important methods are MovingObject.prototype.move, 
+// MovingObject.prototype.draw(ctx), and MovingObject.prototype.isCollidedWith(otherMovingObject)
