6861b878b161a3372108e72457cd69824307d390
Daphne Lam <remnantsofred@gmail.com>
2022-11-29 13:36:59 -0800

    switch to tom

--- src/moving_object.js
+++ src/moving_object.js
@@ -1,3 +1,14 @@
 //Base class for anything that moves.
 // Most important methods are MovingObject.prototype.move, 
 // MovingObject.prototype.draw(ctx), and MovingObject.prototype.isCollidedWith(otherMovingObject)
+
+class MovingObject{
+    constructor(options){
+        this.pos = options["pos"];
+        this.vel = options["vel"];
+        this.radius = options["radius"];
+        this.color = options["color"];
+    } 
+}
+
+module.exports = MovingObject;
